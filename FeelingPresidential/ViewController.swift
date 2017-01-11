52c8598fd74bdc381682951d34eead71d1356281
emsharp <emsharp@ncsu.edu>
2017-01-10 19:17:51 -0500

    Initial Commit

---
+++ FeelingPresidential/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FeelingPresidential
+//
+//  Created by Emily Sharp on 1/10/17.
+//  Copyright © 2017 Emily Sharp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
